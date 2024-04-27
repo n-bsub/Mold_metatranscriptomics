42bef331f74212f22c415d7ee4a27c8191adfb57
n-bsub <neeraja20796@gmail.com>
2024-04-27 15:34:41 -0400

    PCA, PCoA

--- PCoA_PCA/PCA_PCoA_Relative_Taxa_Bray.R
+++ PCoA_PCA/PCA_PCoA_Relative_Taxa_Bray.R
@@ -5,16 +5,14 @@
 #       Balasubrahmaniam et al 2024. Moving Beyond Species: Fungal function 
 #       provides novel targets for potential indicators of mold growth in homes.
 #
-#                       By: Neeraja Balasubrahmaniam
-#
 ################################################################################
 
-# Script name: Rcode_PCoA_Relative_Taxa_Aitchison
+# Script name: Rcode_PCoA_Relative_Taxa_Bray
 # Created on: 24 February 2024
 # Last updated: 26 April 2024
 # Author: Neeraja Balasubrahmaniam and Jon C. King
 # Description: This script performs PCA and PCoA on gene expression and relative
-# abundance taxa (ASVs, species, genus) data using Aitchison distances.
+# abundance taxa (ASVs, species, genus) data using Bray-Curtis distances.
 
 # ---- Setup -------------------------------------------------------------------
 # Install necessary packages
@@ -75,10 +73,9 @@ data_Gen <- data %>%
 data_Spec <- data %>%
   group_by(Species) %>%
   summarise(across(where(is.numeric), sum))
-
-d_list <- list(data_ASV, data_Spec, data_Gen) # combine ASV, species, genus into list
-d_list2 <- list() # initialize
-d_list3 <- list() # initialize
+d_list <- list(data_ASV, data_Spec, data_Gen)
+d_list2 <- list()
+d_list3 <- list()
 for (i in seq(1:3)) {
   d_list2[[i]] <- d_list[[i]] %>%
     select(where(is.numeric)) %>%
@@ -88,27 +85,21 @@ for (i in seq(1:3)) {
                            str_detect(rownames(.), "85") ~ "85%",
                            str_detect(rownames(.), "100") ~ "100%"),
            Site = case_when(str_detect(rownames(.), "CO") ~ "CO",
-                             str_detect(rownames(.), "S1") ~ "OH.1",
-                             str_detect(rownames(.), "S2") ~ "OH.2",
-                             str_detect(rownames(.), "JB") ~ "OH.3",
-                             str_detect(rownames(.), "PA") ~ "PA",
-                             str_detect(rownames(.), "KS") ~ "KS",
-                             str_detect(rownames(.), "SF") ~ "CA",
-                             str_detect(rownames(.), "WA") ~ "WA",
-                             str_detect(rownames(.), "MI") ~ "MI")) %>%
+                            str_detect(rownames(.), "S1") ~ "OH.1",
+                            str_detect(rownames(.), "S2") ~ "OH.2",
+                            str_detect(rownames(.), "JB") ~ "OH.3",
+                            str_detect(rownames(.), "PA") ~ "PA",
+                            str_detect(rownames(.), "KS") ~ "KS",
+                            str_detect(rownames(.), "SF") ~ "CA",
+                            str_detect(rownames(.), "WA") ~ "WA",
+                            str_detect(rownames(.), "MI") ~ "MI")) %>%
     relocate(c("Site","ERH"), .before = everything())
-
-clr <- decostand(d_list2[[i]][,-(1:2)], method = "clr",
-                 MARGIN = 2, pseudocount = 1);          # center log ratio
-
-# center log ratio transformed data with Euclidean distances give Aitchison distances
-
-d_list3[[i]] <- vegdist(clr, method = "euclidean");  # distance matrix
-
+  
 }
 
-names(d_list3) <- c("ASVs", "Species", "Genus") 
-
+# distance matrix: Bray-Curtis
+d_list3 <- lapply(d_list2, function(x) vegdist(x[,-c(1,2)], method = "bray")) 
+names(d_list3) <- c("ASVs", "Species", "Genus")
 
 # PERMANOVA
 for (i in seq(1:3)) {
@@ -119,32 +110,38 @@ for (i in seq(1:3)) {
                 method = "euclidean")) #method = "aitchison", pseudocount=1
 }
 
+
 # Perform PCoA and plot (grouped by ERH)
 
-letters1 = c("A", "C", "E") # initialize for plot labels
+letters1 = c("C", "A", "B") # initialize for plot labels
 
-d_list4 <- list() # initialize
+d_list4 <- list()
 for (i in seq(1:3)) {
-  df <- pcoa(d_list3[[i]])$vectors %>%  # PCoA
+  df <- pcoa(d_list3[[i]])$vectors %>%
     data.frame() %>%
     bind_cols(select(d_list2[[i]], ERH)) %>%
     relocate(c("ERH"), .before = everything())
-  df$ERH <- factor(df$ERH, levels = c("100%", "85%", "50%")) 
-  d_list4[[i]] <- ggplot(df, aes(x = Axis.1,                       # plotting
+  df$ERH <- factor(df$ERH, levels = c("100%", "85%", "50%"))
+  d_list4[[i]] <- #ggplot(data = df) +
+    ggplot(df, aes(x = Axis.1, 
                    y = Axis.2, color = ERH, shape = ERH)) +
-    geom_point(size=3) +
+    geom_point(size=4) +
     scale_shape_manual(values=c(15, 17, 19)) +
     scale_color_manual(values=c('#b91cd7','#009cff', '#00f2aa'))+
     stat_ellipse(geom = "polygon",
                  aes(fill = ERH), 
                  alpha = 0, linewidth = 1) +
+    #geom_point(mapping = aes(x = Axis.1, y = Axis.2, color = RH), size = 3) +
+    #labs(title = names(d_list3)[i], 
     labs(title = paste0("(", letters1[i],") ", "Taxa (", names(d_list3)[i], ")"),      
          x = paste0("PC1 (", 
                     format(round(pcoa(d_list3[[i]])$values$Relative_eig[1],3)*100, nsmall=1),"%)"),
          y = paste0("PC2 (", 
                     format(round(pcoa(d_list3[[i]])$values$Relative_eig[2],3)*100, nsmall=1),"%)")) +
-    xlim(-42, 61) +    #setting plot X axis limits
-    ylim(-56, 46) +    #setting plot Y axis limits
+    #xlim(-1.1, 1.1) +    #setting plot X axis limits for Fig S2
+    #ylim(-0.7, 0.7) +    #setting plot Y axis limits for Fig S2
+    xlim(-0.6,0.82) +    # use this for limits for figure 2 in manuscript
+    ylim(-0.7,0.7) +  # use this for limits for figure 2 in manuscript
     theme(axis.text.y   = element_text(size=18, color = "black", family = "sans"),
           axis.text.x   = element_text(size=18, color = "black", family = "sans"),
           axis.title.y  = element_text(size=18, color = "black", family = "sans"),
@@ -160,14 +157,14 @@ for (i in seq(1:3)) {
     ) +
     coord_fixed(ratio = 1)
 }
-lapply(d_list4, function(x) x)  # show plots; list of 3 plots- ASVs, species, genus
+lapply(d_list4, function(x) x) # show plots; list of 3 plots- ASVs, species, genus
 
 #---- Set working directory to read gene expression raw counts data
 setwd("")
 
 # ---- Load gene expression data for PCA
-data = read.table("RSEM.gene.counts.matrix",               # gene counts table
-                   header=T, com='', row.names=1, check.names=F, sep='\t')
+data = read.table("RSEM.gene.counts.matrix",           # load gene counts table
+                  header=T, com='', row.names=1, check.names=F, sep='\t') 
 # Or load .rds file instead of gene counts table from previous line
 #data = readRDS("RSEM.gene.counts.matrix.rds")
 
@@ -191,7 +188,7 @@ PCA.scores = pca$rotation # these are the points on the graph (use only PC1, PC2
 
 #---- Plotting------------------------------------------------------------------
 
-#---- Get a samples-to-RH file in the same order as colnames of prin_comp_data (samples)
+#---- Get a samples-to-RH file in the same order as colnames of prin_comp_data
 Samples_to_RH=read.table(file="sampletoRH&Site_gene.txt", header = FALSE, sep='	'); 
 Samples_to_RH = data.frame(Samples_to_RH)
 
@@ -200,23 +197,22 @@ PCA.df1 <- as.data.frame(PCA.scores[,c(1,2)]) # only PC1 vs PC2
 df1 = cbind(Samples_to_RH,PCA.df1) # combine RH, Site, RH_level, PC1, PC2
 colnames(df1) <- c("SampleID", "ERH", "RH.level", "Site", "PC1", "PC2") #colnames
 
-# Format label % variance values for plot
+# Format label % variance values
 pretty_pe <- format(round(pc_pct_variance*100, digits =1), nsmall=1, trim=TRUE)
 
 labels <- c(glue("PC1 ({pretty_pe[1]}%)"),
             glue("PC2 ({pretty_pe[2]}%)"))
 
-# Factor ERH to 100%, 85%, 50% order for plot
+# Factor ERH to 100%, 85%, 50% order 
 df1$ERH <- factor(df1$ERH, levels = c("100%", "85%", "50%"))
 
 #---- Plotting PCA grouped by ERH-------------------------------------
-letters2 = c("D", "E", "F")  # change based on plot order preference
-
+letters2 = c("E", "A", "C") # change based on plot order preference
 d_list5 <- list()
 for (j in seq(1:3)) {
   d_list5[[j]] = ggplot(df1, aes(x = PC1, 
                                  y = PC2, color = ERH, shape = ERH)) +
-    geom_point(size=3) +
+    geom_point(size=4) +
     scale_shape_manual(values=c(15, 17, 19)) +
     scale_color_manual(values=c('#b91cd7','#009cff', '#00f2aa'))+
     stat_ellipse(geom = "polygon",
@@ -242,17 +238,15 @@ for (j in seq(1:3)) {
     ) +
     coord_fixed(ratio = 1)
 }
-
-# plot gene expression 3 times to compare each with 3 of taxa PCoA plots
-lapply(d_list5, function(x) x) 
+lapply(d_list5, function(x) x)
 
 
 # PCoA-grouped by Site
 
-letters1 = c("B", "D", "F") # chnage based on prefernce of plot order
+letters1 = c("E", "C", "D") # change based on plot order preference
 
 my_cols = c("#FFD92F", "#FC8D62", "#8DA0CB","#E78AC3","#A6D854","#66C2A5"
-            ,"#00bb00", "#B3B3B3",  "#E5C494")   # color palette for plot
+            ,"#00bb00", "#B3B3B3",  "#E5C494")  
 
 d_list6 <- list()
 for (i in seq(1:3)) {
@@ -261,9 +255,10 @@ for (i in seq(1:3)) {
     bind_cols(select(d_list2[[i]], Site, ERH)) %>%
     relocate(c("Site","ERH"), .before = everything())
   df$ERH <- factor(df$ERH, levels = c("100%", "85%", "50%"))
-  d_list6[[i]] <- ggplot(df, aes(x = Axis.1, 
+  d_list6[[i]] <- 
+    ggplot(df, aes(x = Axis.1, 
                    y = Axis.2, color = Site, shape = ERH)) +
-    geom_point(size=3) +
+    geom_point(size=4) +
     scale_shape_manual(values=c(15, 17, 19)) +
     scale_color_manual(values=my_cols)+
     labs(title = paste0("(", letters1[i],") ", "Taxa (", names(d_list3)[i], ")"),      
@@ -271,10 +266,10 @@ for (i in seq(1:3)) {
                     format(round(pcoa(d_list3[[i]])$values$Relative_eig[1],3)*100, nsmall=1),"%)"),
          y = paste0("PC2 (", 
                     format(round(pcoa(d_list3[[i]])$values$Relative_eig[2],3)*100, nsmall=1),"%)")) +
-    xlim(-42, 61) +    #setting plot X axis limits-use for combined plot
-    ylim(-56, 46) + #setting plot Y axis limits-use for combined plot
-    #xlim(-30,50) +    #older limits if plotting this as standalone
-    #ylim(-46, 28) +    #older limits if plotting this as standalone
+    #xlim(-1.1, 1.1) +    #setting plot X axis limits for Fig S2
+    #ylim(-0.7, 0.7) +    #setting plot Y axis limits for Fig S2
+    xlim(-0.6,0.82) +    # use this for limits for figure 2 in manuscript
+    ylim(-0.7,0.7) +  # use this for limits for figure 2 in manuscript
     theme(axis.text.y   = element_text(size=18, color = "black", family = "sans"),
           axis.text.x   = element_text(size=18, color = "black", family = "sans"),
           axis.title.y  = element_text(size=18, color = "black", family = "sans"),
@@ -294,12 +289,12 @@ lapply(d_list6, function(x) x)
 
 
 #---- Plotting PCA grouped by Site-------------------------------------
-letters2 = c("A", "C", "E")
+letters2 = c("E", "A", "C") # change based on plot order preference
 d_list7 <- list()
 for (j in seq(1:3)) {
   d_list7[[j]] = ggplot(df1, aes(x = PC1, 
                                  y = PC2, color = Site, shape = ERH)) +
-    geom_point(size=3) +
+    geom_point(size=4) +
     scale_shape_manual(values=c(15, 17, 19)) +
     scale_color_manual(values=my_cols)+
     labs(title = paste0("(", letters2[j],") ", "Gene Expression"))+
@@ -324,25 +319,38 @@ for (j in seq(1:3)) {
 }
 lapply(d_list7, function(x) x)
 
-# To plot figure S3
+# To plot figure S2
 p6 = ggdraw() +
-  draw_plot(d_list4[[1]], x = 0, y = 0.67, width = .5, height = .33) +
+  draw_plot(d_list4[[2]], x = 0, y = 0.5, width = .5, height = 0.5) +
   
-  draw_plot(d_list4[[2]], x = 0, y = 0.33, width = .5, height = 0.33) +
+  draw_plot(d_list4[[3]], x = 0.5, y = 0.5, width = .5, height = 0.5) +
+
+  draw_plot(d_list6[[2]], x = 0, y = 0, width = .5, height = 0.5) +
   
-  draw_plot(d_list4[[3]], x = 0, y = 0, width = .5, height = 0.33) +
-  
-  draw_plot(d_list6[[1]], x = .5, y = 0.67, width = .5, height = .33) +
-  
-  draw_plot(d_list6[[2]], x = .5, y = 0.33, width = .5, height = .33) +
-  
-  draw_plot(d_list6[[3]], x = .5, y = 0, width = .5, height = 0.33) 
+  draw_plot(d_list6[[3]], x = .5, y = 0, width = .5, height = 0.5) 
 
 p6
 
+# To plot figure 3: 
+# use limits mentioned for figure 2 previously before plotting this
+# modify plot labels (A, B, C, D) using Inkscape 
+
+p7 = ggdraw() +
+  draw_plot(d_list5[[1]], x = 0, y = 0.5, width = .5, height = 0.5) +
+  
+  draw_plot(d_list4[[1]], x = 0.5, y = 0.5, width = .5, height = 0.5) +
+  
+  draw_plot(d_list7[[1]], x = 0, y = 0, width = .5, height = 0.5) +
+  
+  draw_plot(d_list6[[1]], x = .5, y = 0, width = .5, height = 0.5) 
+
+p7  
+  
+
+
 # To Save as pdf, provide path within quotes
 
-#pdf(file = "", height = 14, width = 12) 
+#pdf(file = "", height = 11, width = 16)
 
 #p6
 
