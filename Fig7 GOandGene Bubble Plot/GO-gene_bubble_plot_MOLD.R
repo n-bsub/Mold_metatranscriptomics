7aa85c44b6df850c66ed39b1116a55717a6b427a
n-bsub <neeraja20796@gmail.com>
2024-04-28 14:03:03 -0400

    GO-gene log2fc bubble plot

--- Fig7 GOandGene Bubble Plot/GO-gene_bubble_plot_MOLD.R
+++ Fig7 GOandGene Bubble Plot/GO-gene_bubble_plot_MOLD.R
@@ -44,7 +44,7 @@ packageVersion("ggplot2")
 
 #---- Code to create bubble plots of log2FC data--------------------------------
 # log2FC and FDR-adjusted p values of all DE genes, their fungal annotations
-# are provided in a separate .txt file
+# are provided in a separate .txt file as well as in SI Table S10 
 
 #---- Set working directory to read data
 setwd("")
@@ -56,6 +56,7 @@ df1 <- data.frame(data1)
 
 df1 <- data.frame(data1)
 df1$log2FC = as.numeric(df1$log2FC)
+
 df1 <- df1[ order(df1$gene, decreasing = TRUE), ]
 
 df1$term.group=factor(df1$term.group,levels = c("Morphological", 
@@ -70,6 +71,7 @@ data2=read.table(file="group2_go_gene_longer.txt", header = TRUE, sep='	');
 
 df2 <- data.frame(data2)
 df2$log2FC = as.numeric(df2$log2FC)
+
 df2 <- df2[ order(df2$gene, decreasing = TRUE), ]
 
 df2$term.group=factor(df2$term.group,levels = c("Morphological", 
@@ -83,7 +85,7 @@ data3=read.table(file="group3_go_gene_longer.txt", header = TRUE, sep='	');
 
 df3 <- data.frame(data3)
 df3$log2FC = as.numeric(df3$log2FC)
-df3 = df3[,-1]
+
 df3 <- df3[ order(df3$gene, decreasing = TRUE), ]
 
 df3$term.group=factor(df3$term.group,levels = c("Morphological", 
@@ -101,15 +103,12 @@ p1 = ggplot(df1, aes(x=term.group , y=gene)) +
   theme_bw() +
   theme(axis.text.x = element_text(size=30, color = "black", family = "sans", 
                                    angle = 30, vjust = 1, hjust=0.9),
-        #axis.text.x = element_blank(),
         axis.text.y = element_text(size=30, color = "black", family = "sans"),
         legend.text = element_text(size=30, color = "black", family = "sans"),
         legend.title = element_text(size=30, color = "black", family = "sans"),
         panel.background = element_blank(),
         panel.grid.major = element_line(linewidth = 0.25),
         panel.grid.minor = element_line(linewidth = 0.25),
-        #panel.grid.major = element_blank(), 
-        #panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
         legend.position = "right") +
   guides(fill = guide_legend(override.aes = list(size = 10)))
@@ -123,15 +122,12 @@ p2 = ggplot(df2, aes(x=term.group , y=gene)) +
   theme_bw() +
   theme(axis.text.x = element_text(size=30, color = "black", family = "sans", 
                                    angle = 30, vjust = 1, hjust=0.9),
-        #axis.text.x = element_blank(),
         axis.text.y = element_text(size=30, color = "black", family = "sans"),
         legend.text = element_text(size=30, color = "black", family = "sans"),
         legend.title = element_text(size=30, color = "black", family = "sans"),
         panel.background = element_blank(),
         panel.grid.major = element_line(linewidth = 0.25),
         panel.grid.minor = element_line(linewidth = 0.25),
-        #panel.grid.major = element_blank(), 
-        #panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
         legend.position = "right") +
   guides(fill = guide_legend(override.aes = list(size = 10)))
@@ -145,15 +141,12 @@ p3 = ggplot(df3, aes(x=term.group , y=gene)) +
   theme_bw() +
   theme(axis.text.x = element_text(size=30, color = "black", family = "sans", 
                                    angle = 30, vjust = 1, hjust=0.9),
-        #axis.text.x = element_blank(),
         axis.text.y = element_text(size=30, color = "black", family = "sans"),
         legend.text = element_text(size=30, color = "black", family = "sans"),
         legend.title = element_text(size=30, color = "black", family = "sans"),
         panel.background = element_blank(),
         panel.grid.major = element_line(linewidth = 0.25),
         panel.grid.minor = element_line(linewidth = 0.25),
-        #panel.grid.major = element_blank(), 
-        #panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
         legend.position = "right") +
   guides(fill = guide_legend(override.aes = list(size = 10)))
@@ -164,6 +157,6 @@ p4 = p1 + p2 + p3 + plot_layout(guides = 'collect')
 p4
 
 #---- Save combined plot p4 as PDF----------------------------------------------
-pdf(file = "", height = 14, width = 20)
+pdf(file = "GO-gene-bubble.pdf", height = 14, width = 20)
 p4
 dev.off()
